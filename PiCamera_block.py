18cc7528afef97563d405ccad9986d80961fa28e
Scott Clay <sclay@n.io>
2018-01-25 15:33:26 -0700

    add file name

--- PiCamera_block.py
+++ PiCamera_block.py
@@ -1,12 +1,15 @@
+from datetime import datetime
 from picamera import PiCamera
 
 from nio.block.base import Block
-from nio.properties import VersionProperty
+from nio.properties import VersionProperty, FileProperty 
 
 
 class Picamera(Block):
 
     version = VersionProperty('0.1.0')
+    image = FileProperty(title='Image', default='image')
+    count = 0
 
     def configure(self, context):
     	super().configure(context)
@@ -14,5 +17,7 @@ class Picamera(Block):
 
     def process_signals(self, signals):
         for signal in signals:
-            self.camera.capture('image.jpg')
+            image_name = '{}_{}'.format(self.image, self.count)
+            self.camera.capture('{}.jpg'.format(image_name))
+            self.count += 1
         self.notify_signals(signals)
